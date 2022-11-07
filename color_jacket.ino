602a6d8d812c829045231be76a64a820840f7242
yuuuuuta1118 <yuta.hori1118@gmail.com>
2022-11-08 00:33:36 +0900

    add down pattern

--- color_jacket.ino
+++ color_jacket.ino
@@ -15,32 +15,38 @@ static bool bleStart = true;
 
 #define Neopixel_PIN 33 // M5Stack Grobe Pin
 #define NUMPIXELS  360  // Enough for 29 LEDs
+#define NUMPIXELLINES 20
 
 int LED_PATTERN_LOVE_HEART[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 84, 85, 86, 87, 88, 89, 107, 108, 109, 116, 117, 121, 122, 133, 134, 143, 144, 156, 157, 164, 165, 166, 177, 178, 181, 182, 193, 194, 195, 202, 203, 215, 216, 225, 226, 237, 238, 242, 243, 250, 251, 252, 270, 271, 272, 273, 274, 275, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 342, 343, 344, 345, 346, 347, 348, 349, 350, 351, 352, 353, 354, 355, 356, 357};
-int LED_PATTERN_DREAM[] = {82, 89, 90, 91, 92, 106, 107, 108, 109, 110, 112, 113, 114, 117, 122, 125, 127, 129, 132, 133, 144, 145, 146, 147, 148, 150, 152, 153, 154, 156, 157, 158, 159, 162, 165, 167, 169, 171, 175, 176, 177, 180, 181, 182, 184, 185, 188, 190, 192, 194, 197, 200, 201, 202, 203, 205, 206, 207, 209, 210, 211, 212, 213, 214, 219, 221, 222, 223, 224, 225, 230, 232, 234, 237, 242, 245, 246, 247, 249, 250, 251, 252, 267, 268, 269, 270, 277}
+int LED_PATTERN_DREAM[] = {82, 89, 90, 91, 92, 106, 107, 108, 109, 110, 112, 113, 114, 117, 122, 125, 127, 129, 132, 133, 144, 145, 146, 147, 148, 150, 152, 153, 154, 156, 157, 158, 159, 162, 165, 167, 169, 171, 175, 176, 177, 180, 181, 182, 184, 185, 188, 190, 192, 194, 197, 200, 201, 202, 203, 205, 206, 207, 209, 210, 211, 212, 213, 214, 219, 221, 222, 223, 224, 225, 230, 232, 234, 237, 242, 245, 246, 247, 249, 250, 251, 252, 267, 268, 269, 270, 277};
 int LED_PATTERN_Z[] = {82, 83, 84, 97, 98, 99, 100, 101, 102, 112, 113, 114, 115, 116, 117, 122, 123, 124, 125, 126, 127, 137, 138, 139, 140, 141, 142, 149, 150, 151, 155, 156, 157, 162, 163, 164, 168, 169, 170, 177, 178, 179, 180, 181, 182, 186, 187, 188, 195, 196, 197, 202, 203, 204, 211, 212, 213, 217, 218, 219, 220, 221, 222, 223, 224, 225, 235, 236, 237, 242, 243, 244, 254, 255, 256, 257, 258, 259, 260, 261, 262, 275, 276, 277};
 int LED_PATTERN_HEART[] = {84, 85, 86, 87, 88, 89, 107, 108, 109, 116, 117, 121, 122, 133, 134, 143, 144, 156, 157, 164, 165, 166, 177, 178, 181, 182, 193, 194, 195, 202, 203, 215, 216, 225, 226, 237, 238, 242, 243, 250, 251, 252, 270, 271, 272, 273, 274, 275};
+int step_num = 0;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, Neopixel_PIN, NEO_GRB + NEO_KHZ800);
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
-       Serial.print("Advertised Device found: ");
+      //  Serial.print("Advertised Device found: ");
        if(advertisedDevice.haveServiceUUID()){
             //if(strncmp(advertisedDevice.getServiceUUID().toString().c_str(),uuid, 36) == 0){
-            Serial.print("Advertised Device found: ");
-            Serial.println(advertisedDevice.toString().c_str());
+            // Serial.print("Advertised Device found: ");
+            // Serial.println(advertisedDevice.toString().c_str());
             if(strncmp(advertisedDevice.getServiceUUID().toString().c_str(),down_uuid, 36) == 0){
+                Serial.println(advertisedDevice.getServiceUUID().toString().c_str());
                 //M5.Lcd.printf("getUUID: %s\r\n", advertisedDevice.getServiceUUID().toString().c_str());
                 device = new BLEAdvertisedDevice(advertisedDevice);
                 doConnect = "down";
             } else if(strncmp(advertisedDevice.getServiceUUID().toString().c_str(),stop_uuid, 36) == 0){
+                Serial.println(advertisedDevice.getServiceUUID().toString().c_str());
                 device = new BLEAdvertisedDevice(advertisedDevice);
                 doConnect = "stop";
             } else if(strncmp(advertisedDevice.getServiceUUID().toString().c_str(),up_uuid, 36) == 0){
+                Serial.println(advertisedDevice.getServiceUUID().toString().c_str());
                 device = new BLEAdvertisedDevice(advertisedDevice);
                 doConnect = "up";
             } else if(strncmp(advertisedDevice.getServiceUUID().toString().c_str(),denger_uuid, 36) == 0){
+                Serial.println(advertisedDevice.getServiceUUID().toString().c_str());
                 device = new BLEAdvertisedDevice(advertisedDevice);
                 doConnect = "denger";
             } else {
@@ -95,7 +101,7 @@ void drawScreen() {
     M5.Lcd.setTextColor(WHITE);
     //M5.Lcd.printf(" %2d",deviceNum);
 
-    M5.Lcd.printf("%s", doConnect ? "true" : "false");
+    M5.Lcd.printf("%s", doConnect);
 
 }
 
@@ -125,29 +131,33 @@ void setup() {
 }
 
 void setPattern(String pattern){
+  Serial.println(pattern);
   int pattern_index = 0;
-  if(pattern.compareTo("down")){
+  if(pattern == "down"){
+    int pixelHue = 0;
     for(int i=0; i<NUMPIXELS; i++){
-      if(i == LED_PATTERN_DREAM[pattern_index]){
-        pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-        pattern_index++;
+      if((i<20 || i>339)&& i%10 == 0){
+        pixelHue = step_num + (i * 65536L / NUMPIXELLINES);        
+      }else if(i>=20 && i<=239 && i%20 == 0){
+        pixelHue = step_num + (i * 65536L / NUMPIXELLINES); 
       }
+      pixels.setPixelColor(i, pixels.gamma32(pixels.ColorHSV(pixelHue)));
     }
-  } else if(pattern.compareTo("stop")){
+  } else if(pattern == "stop"){
     for(int i=0; i<NUMPIXELS; i++){
       if(i == LED_PATTERN_LOVE_HEART[pattern_index]){
         pixels.setPixelColor(i, pixels.Color(0, 150, 0));
         pattern_index++;
       }
     }
-  } else if(pattern.compareTo("up")){
+  } else if(pattern == "up"){
     for(int i=0; i<NUMPIXELS; i++){
       if(i == LED_PATTERN_Z[pattern_index]){
         pixels.setPixelColor(i, pixels.Color(0, 150, 0));
         pattern_index++;
       }
     }
-  } else if(pattern.compareTo("denger")){
+  } else if(pattern == "denger"){
     for(int i=0; i<NUMPIXELS; i++){
       // if(i == LED_PATTERN_Z[pattern_index]){
         pixels.setPixelColor(i, pixels.Color(150, 0, 0));
@@ -167,6 +177,10 @@ void lightPixcel(String onLight){
   pixels.setBrightness(100);
   setPattern(onLight);
   pixels.show();
+  step_num += 2048;
+  if (step_num >= 65536) {
+    step_num = 0;
+  }
 }
 
 void loop() {
